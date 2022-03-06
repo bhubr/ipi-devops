9bca0458db5751e963d8732d38417c435d15876e
Benoît Hubert <benoithubert@gmail.com>
2022-03-06 12:29:00 +0100

    Ansible playbook continued on Feb 28th 2022

---
+++ 28fev22/templates/ecosystem.config.js
@@ -0,0 +1,13 @@
+// ecosystem.config.js
+// https://stackoverflow.com/q/60581617
+
+module.exports = {
+  apps : [{
+    name: 'node-album-api',
+    // On doit passer directement le chemin vers l'"exécutable" ts-node
+    script: './node_modules/.bin/ts-node',
+    // Paramètres recommandés pour le fonctionnement en production
+    // (Diminue l'empreinte mémoire)
+    args: '--transpile-only src/index.ts',
+  }],
+};
